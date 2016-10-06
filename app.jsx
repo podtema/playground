f68c2f22e931e7bb53250cd8cdd78c9346f21500
Artem Rosnovskiy <inbox@podtema.com>
2016-10-06 00:29:48 -0700

    Setup process

--- app.jsx
+++ app.jsx
@@ -1,8 +1,8 @@
 function Application() {
 	return (
-		<div>
+		<div className="">
 			<h1>This is just a test</h1>
-			<p className="lead">Here we'll do all sorts of tests. Nothing significant yet, but wait untill we geat to the meat of this whole ReactJS thing!</p>
+			<p className="lead">Here we'll do all sorts of tests. Nothing significant yet, but wait untill we geat to the meat of this whole <mark>ReactJS</mark> thing!</p>
 		</div>
 		);
 
