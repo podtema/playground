0e8f97e4484755f6b176541e12c1f666cfe849f4
Artem Rosnovskiy <inbox@podtema.com>
2016-10-06 00:44:30 -0700

    init app layout

--- app.jsx
+++ app.jsx
@@ -1,8 +1,34 @@
 function Application() {
 	return (
-		<div className="">
-			<h1>This is just a test</h1>
-			<p className="lead">Here we'll do all sorts of tests. Nothing significant yet, but wait untill we geat to the meat of this whole <mark>ReactJS</mark> thing!</p>
+		<div className="app-container">
+			
+			<div className="row">
+				<div className="col-md-3"></div>
+				<div className="col-md-6">
+					<h1 className="text-center">App Heading and Title</h1>
+					<h1 className="text-center"><small>and subtitle</small></h1>
+				</div>
+				<div className="col-md-3"></div>
+			</div>
+			
+			<div className="row content">
+				<div className="col-md-3"></div>
+				<div className="col-md-6">
+					<h3 className="text-center">App content</h3>
+					<h3 className="text-center"><small>and more content</small></h3>
+				</div>
+				<div className="col-md-3"></div>
+			</div>
+
+			
+			<div className="row">
+				<div className="col-md-3"></div>
+				<div className="col-md-6">
+					<p className="text-center lead">App footer</p>
+					<p className="text-center"><small>© 2016</small></p>
+				</div>
+				<div className="col-md-3"></div>
+			</div>
 		</div>
 		);
 
