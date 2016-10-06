ec106d47dde3f578022a0259db810d90d3a9c45a
Artem Rosnovskiy <inbox@podtema.com>
2016-10-05 23:59:21 -0700

    Initial commit

---
+++ app.jsx
@@ -0,0 +1,11 @@
+function Application() {
+	return (
+		<div>
+			<h1>This is just a test</h1>
+			<p className="lead">Here we'll do all sorts of tests. Nothing significant yet, but wait untill we geat to the meat of this whole ReactJS thing!</p>
+		</div>
+		);
+
+}
+
+ReactDOM.render(<Application />, document.getElementById('react-app'));
